8a48591bd9dec7b017c266dbebfc35ae10ebf10b
alumno <alumno@A302PC36>
2022-11-04 14:05:44 +0100

    Agregar archivos de proyecto.

---
+++ Superheroes/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Superheroes.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Superheroes
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int pestaña = 1;
+        int numSuper = 0;
+        SolidColorBrush cl = new SolidColorBrush();
+
+        List<Superheroe> sh = new List<Superheroe>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            sh = Superheroe.GetSamples();
+            contenedorDockPanel.DataContext = sh[numSuper];
+
+            cambioHeroe();
+        }
+
+        public void cambioHeroe()
+        {
+            if (((Superheroe)contenedorDockPanel.DataContext).Vengador == true)
+                compañiaPertenecienteImage.Visibility = Visibility.Visible;
+            else
+                compañiaPertenecienteImage.Visibility = Visibility.Collapsed;
+
+            if (((Superheroe)contenedorDockPanel.DataContext).Xmen == true)
+                compañiaPerteneciente2Image.Visibility = Visibility.Visible;
+            else
+                compañiaPerteneciente2Image.Visibility = Visibility.Collapsed;
+            
+            if (((Superheroe)contenedorDockPanel.DataContext).Heroe == true)
+            {
+                cl.Color = Colors.LightGreen;
+                contenedorDockPanel.Background = cl;
+            }
+            else
+            {
+                cl.Color = Colors.IndianRed;
+                contenedorDockPanel.Background = cl;
+            }
+                
+        }
+
+        private void leftArrowImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (pestaña > 1)
+            {
+                pestaña--;
+                numSuper--;
+
+                guiaPaginaTextBlock.Text = $"{pestaña}/3";
+                contenedorDockPanel.DataContext = sh[numSuper];
+                cambioHeroe();
+            }  
+        }
+
+        private void rightArrowImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (pestaña < 3)
+            {
+                pestaña++;
+                numSuper++;
+
+                guiaPaginaTextBlock.Text = $"{pestaña}/3";
+                contenedorDockPanel.DataContext = sh[numSuper];
+                cambioHeroe();
+            }
+        }
+    }
+}
---
+++ Superheroes/MainWindowVM.cs
@@ -0,0 +1,72 @@
+﻿using Superheroes.models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheroes
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Superheroe heroeActual;
+        public Superheroe HeroeActual
+        {
+            get { return heroeActual; }
+            set { 
+                heroeActual = value;
+                NotifyPropertyChanged("HeroeActual");
+            }
+        }
+
+        private int contadorActual;
+        public int ContadorActual
+        {
+            get { return contadorActual; }
+            set { 
+                contadorActual = value;
+                NotifyPropertyChanged("ContadorActual");
+            }
+        }
+
+        private int totalHeroes;
+        public int TotalHeroes
+        {
+            get { return totalHeroes; }
+            set {
+                totalHeroes = value;
+                NotifyPropertyChanged("TotalHeroes");
+            }
+        }
+
+        List<Superheroe> lista;
+
+        public MainWindowVM()
+        {
+            contadorActual = 1;
+            totalHeroes = 3;
+            HeroeActual = lista[contadorActual - 1];
+            TotalHeroes = lista.Count;
+        }
+
+        public void Avanzar()
+        {
+            ContadorActual++;
+            HeroeActual = lista[contadorActual - 1];
+        }
+
+        public void Retroceder()
+        {
+            ContadorActual--;
+            HeroeActual = lista[contadorActual - 1];
+        }
+
+    }
+}
